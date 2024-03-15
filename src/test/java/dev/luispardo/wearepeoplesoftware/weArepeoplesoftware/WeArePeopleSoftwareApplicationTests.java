332e817859c9e8f2fe67c688c9e81fd725b9d4a8
Luis Pardo <luispardosuarez@gmail.com>
2024-03-15 20:20:30 +0100

    Added application properties, application-mysql properties, auth controller, and security configuration files

---
+++ src/test/java/dev/luispardo/wearepeoplesoftware/weArepeoplesoftware/WeArePeopleSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luispardo.wearepeoplesoftware.weArepeoplesoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeArePeopleSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
