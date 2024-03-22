f2856c32d5668ac59cbe3b233fcc7b9bf275a229
Luis Pardo <luispardosuarez@gmail.com>
2024-03-22 18:10:22 +0100

    Define LoginRequest for user authentication

---
+++ src/main/java/dev/luispardo/wearepeoplesoftware/security/JwtTokenProvider.java
@@ -0,0 +1,61 @@
+package dev.luispardo.wearepeoplesoftware.security;
+
+import dev.luispardo.wearepeoplesoftware.model.Usuario;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${app.jwtExpirationInMs}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication) {
+
+        Usuario userPrincipal = (Usuario) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(Long.toString(userPrincipal.getId()))
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException ex) {
+            // Log exception
+        } catch (MalformedJwtException ex) {
+            // Log exception
+        } catch (ExpiredJwtException ex) {
+            // Log exception
+        } catch (UnsupportedJwtException ex) {
+            // Log exception
+        } catch (IllegalArgumentException ex) {
+            // Log exception
+        }
+        return false;
+    }
+}
