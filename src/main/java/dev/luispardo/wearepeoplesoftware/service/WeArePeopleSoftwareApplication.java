de16c76dd2432d24512574f523454ffb530eb9d5
Luis Pardo <luispardosuarez@gmail.com>
2024-03-22 18:13:17 +0100

    Implement WeArePeopleSoftwareApplication.java for Spring Boot application setup

---
+++ src/main/java/dev/luispardo/wearepeoplesoftware/service/WeArePeopleSoftwareApplication.java
@@ -0,0 +1,14 @@
+package dev.luispardo.wearepeoplesoftware.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeArePeopleSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeArePeopleSoftwareApplication.class, args);
+	}
+
+}
+
